2736c4261bbb737a12bb50c19507f8de3f6522f8
a-t-em <107615653+a-t-em@users.noreply.github.com>
2024-07-04 12:18:07 +0000

    successful assertion

--- 06-best-practices/homework/tests/test_batch.py
+++ 06-best-practices/homework/tests/test_batch.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from datetime import datetime
+import batch
 from batch import prepare_data
-
+import pickle
 
 def dt(hour, minute, second=0):
     return datetime(2023, 1, 1, hour, minute, second)
@@ -17,12 +18,29 @@ columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff
 df = pd.DataFrame(data, columns=columns)
 
 ans = [
-    (-1, -1, dt(1, 1), dt(1, 10), 9),
-    (1, 1, dt(1, 2), dt(1, 10), 8),
+    (-1, -1, dt(1, 1), dt(1, 10)),
+    (1, 1, dt(1, 2), dt(1, 10)),
 ]
 
-df_ans = pd.DataFrame(ans, columns=columns.append('duration'))
-df_prep = prepare_data(df, columns)
+df_ans = pd.DataFrame(ans, columns=columns)
+df_ans['duration'] = [9.0, 8.0]
 
-assert df_ans == df_prep
+df_prep = prepare_data(df, columns)
+df_prep['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'], format='%Y-%m-%d %H:%M:%S')
+df_prep['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'], format='%Y-%m-%d %H:%M:%S')
+df_prep['PULocationID'] = df_prep['PULocationID'].astype(int)
+df_prep['DOLocationID'] = df_prep['DOLocationID'].astype(int)
+
+# with open('model.bin', 'rb') as f_in:
+#     dv, lr = pickle.load(f_in)
+
+# categorical = ['PULocationID', 'DOLocationID']
+
+# dicts = df[categorical].to_dict(orient='records')
+# X_val = dv.transform(dicts)
+# y_pred = lr.predict(X_val)
+
+# print('predicted total duration:', sum(y_pred))
+
+assert df_ans.equals(df_prep)
 
